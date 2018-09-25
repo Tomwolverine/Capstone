9e11a17e0e4f7d99c506a909f84015ba80f528f0
tom lee <tdroid99@gmail.com>
2018-09-25 14:28:34 -0600

    data for prefab

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+	[SerializeField]int health;
+	[SerializeField]int arrowDamage;
+	[SerializeField]float moveSpeed;
+
+	void TakeDamage(int damage) {
+		health -= damage;
+		if(health < 1) {
+			Destroy(gameObject);
+		}
+    }	
+
+	void OnTriggerEnter(Collider col) {
+		var target = col.gameObject.name;
+		if(target.Contains("Arrow")) {
+			TakeDamage(arrowDamage);
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
